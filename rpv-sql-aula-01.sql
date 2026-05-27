9982c1a2a47a17326f37237e4463fcb55b49a820
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-27 19:20:52 -0300

    feat: add new features for exercise 04

--- rpv-sql-aula-01.sql
+++ rpv-sql-aula-01.sql
@@ -377,12 +377,20 @@ ALTER TABLE tbl_pessoas CHANGE COLUMN Prof Profissao_Coluna VARCHAR(40);
 # Exercício 04
 ##################################################################################
 
+CREATE DATABASE db_livraria;
+
 USE db_livraria;
 
 ALTER TABLE tbl_livro
 ADD CONSTRAINT fk_id_categoria
 FOREIGN KEY (ID_Categoria)
 REFERENCES tbl_categorias (ID_Categoria);
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Prentice Hall');
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('O ´Reilly');
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Microsoft Press');
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Wiley');
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('McGraw-Hill Education');
 
 INSERT INTO tbl_autores
 VALUES
@@ -409,14 +417,16 @@ VALUES
 (4,"Astronomia"),
 (5,"Botânica");
 
-INSERT INTO tbl_Livro (Nome_Livro, ISBN13, ISBN10, Data_Pub, Preco_Livro, ID_Categoria, ID_Autor, ID_Editora)​
-VALUES​
-('Linux Command Line and Shell Scripting','9781118983843', '111898384X', '20150109', 68.35, 1, 5, 4),​
-('SSH, the Secure Shell','9780596008956', '0596008953', '20050517', 58.30, 1, 1, 2),​
-('Using Samba','9780596002565', '0596002564', '20031221', 61.45, 1, 2, 2),​
-('Fedora and Red Hat Linux', '9780133477436', '0133477436', '20140110', 62.24, 1, 3, 1),​
-('Windows Server 2012 Inside Out','9780735666313', '0735666318', '20130125', 66.80, 1, 4, 3),​
-('Microsoft Exchange Server 2010','9780735640610', '0735640610', '20101201', 45.30, 1, 4, 3),​
+INSERT INTO tbl_Livro (Nome_Livro, ISBN13, ISBN10, Data_Pub, Preco_Livro, ID_Categoria,
+ID_Autor, ID_Editora)
+VALUES
+('Linux Command Line and Shell Scripting','9781118983843', '111898384X', '20150109', 68.35, 1, 5,
+4),
+('SSH, the Secure Shell','9780596008956', '0596008953', '20050517', 58.30, 1, 1, 2),
+('Using Samba','9780596002565', '0596002564', '20031221', 61.45, 1, 2, 2),
+('Fedora and Red Hat Linux', '9780133477436', '0133477436', '20140110', 62.24, 1, 3, 1),
+('Windows Server 2012 Inside Out','9780735666313', '0735666318', '20130125', 66.80, 1, 4, 3),
+('Microsoft Exchange Server 2010','9780735640610', '0735640610', '20101201', 45.30, 1, 4, 3),
 ('Practical Electronics for Inventors', '9781259587542', '1259587541', '20160324', 67.80, 1, 13, 5);
 
 SELECT * FROM tbl_autores;
